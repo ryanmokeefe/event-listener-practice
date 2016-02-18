b2a8b24903b81b0c5b1ea02ff62ac6e32c9e9ffa
Adrian Maseda <amaseda@gmail.com>
2016-02-18 15:49:16 -0500

    add starter code

---
+++ script.js
@@ -0,0 +1,12 @@
+div.eventListener("click", changeBackgroundColorToBlue());
+div.eventListener("click", changeBackgroundColorToWhite());
+
+function changeBackgroundColorToBlue(){
+  // No need to change the content of this function. Don't worry if this code is new to you.
+  document.body.style.backgroundColor = "blue";
+}
+
+function changeBackgroundColorToWhite(){
+  // No need to change the content of this function. Don't worry if this code is new to you.
+  document.body.style.backgroundColor = "white";
+}
