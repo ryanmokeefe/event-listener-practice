9ae7b6d86db7bd2143f98ad53cb326b36b6cf339
Nick Olds <nolds9@yahoo.com>
2016-05-30 20:22:45 -0400

    adds jQuery starter

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-div.eventListener("click", changeBackgroundColorToBlue());
-div.eventListener("click", changeBackgroundColorToWhite());
+div.onclick(changeBackgroundColorToBlue());
+div.onclick(changeBackgroundColorToWhite());
 
 function changeBackgroundColorToBlue(){
   // No need to change the content of this function. Don't worry if this code is new to you.
-  document.body.style.backgroundColor = "blue";
+  $("body").css("background", "blue");
 }
 
 function changeBackgroundColorToWhite(){
   // No need to change the content of this function. Don't worry if this code is new to you.
-  document.body.style.backgroundColor = "white";
+  $("body").css("background", "white");
 }
