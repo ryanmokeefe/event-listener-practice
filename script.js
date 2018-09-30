8f960ca42f676a7509fc02438219da2161e0459c
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-09-29 22:19:38 -0400

    backup

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function changeBackgroundColorToRed() {
   // No need to change the content of this function. Don't worry if this code is new to you.
   document.body.style.backgroundColor = 'red';
 }
+
+let buttonRed = document.getElementsByClassName("red")[0]
+let buttonBlue = document.getElementsByClassName("blue")[0]
+
+buttonRed.addEventListener('click', changeBackgroundColorToRed)
+  buttonRed.addEventListener('mouseover', changeBackgroundColorToRed)
+
+
+buttonBlue.addEventListener('click', changeBackgroundColorToBlue)
+
+  buttonBlue.addEventListener('mouseover', changeBackgroundColorToBlue)
+
+
